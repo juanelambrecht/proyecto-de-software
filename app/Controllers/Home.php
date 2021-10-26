a910f2a6ca1d0bbf64e12764bb6165ddae17ce3a
Matias <matiasdmitrowicz@gmail.com>
2021-10-26 19:12:17 -0300

    edit m

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,6 +7,6 @@ class Home extends BaseController
     public function index()
     {
         return redirect()->to('welcome_message');
-        //edit por mati v2
+        //edit por mati v3
     }
 }
