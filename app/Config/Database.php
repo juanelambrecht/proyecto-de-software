1d433c8cff2023629a96808c3d2988645042dea7
AlexisGomez99 <ralexisge@gmail.com>
2021-10-28 15:57:34 -0300

    base de datos

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'usuarios',
+        'database' => 'proyecto_gdl',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
