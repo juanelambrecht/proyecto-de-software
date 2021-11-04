85e156a37018f39ca10365b31b9cd868a3272569
AlexisGomez99 <ralexisge@gmail.com>
2021-11-04 15:53:41 -0300

    cambio

--- app/Views/template/inspector-template.php
+++ app/Views/template/inspector-template.php
@@ -117,7 +117,7 @@ $nav_venderEstadia = false;
             <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block">Usuario Administrador</a>
+            <a href="#" class="d-block">Usuario Inspector</a>
           </div>
         </div>
 
