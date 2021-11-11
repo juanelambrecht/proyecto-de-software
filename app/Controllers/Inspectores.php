f1d579b57e37fbe963ec1a49975d4ff61afa3abc
AlexisGomez99 <ralexisge@gmail.com>
2021-11-11 16:42:06 -0300

    ASD

--- app/Controllers/Inspectores.php
+++ app/Controllers/Inspectores.php
@@ -10,7 +10,7 @@ class Inspectores extends BaseController
     public function listarEstacionamiento()
     {
         $estadia = new Estadia();
-        $datos['estadias'] = $estadia->orderBy('id', 'ASC')->findAll();
+        $datos['estadias'] = $estadia->orderBy('id', 'DESC')->findAll();
         $usuario = new Usuario();
         $datos['usuarios'] = $usuario->orderBy('id', 'ASC')->findAll();
         return view('inspectores/consultaEstacionamiento', $datos);
